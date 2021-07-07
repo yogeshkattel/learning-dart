9ec32b2b46e2339f06b61e1851e4fb92b2df5a32
yogeshkattel <yogeshkattel66@gmail.com>
2021-07-07 14:02:00 +0545

    fdf

---
+++ datatype.dart
@@ -0,0 +1,18 @@
+void main() {
+  //string data type
+  String name = "yogesh kattel";
+  // integer data type
+  int age = 18;
+  // double data type
+  double fees = 200.00;
+  //boolean data type
+  bool isworking = false;
+
+  //list
+  List<String> friends = ['roshan', 'pranjal', 'ronish', 'kabish', 'yogesh'];
+  //date time
+  DateTime dob = DateTime(2060, 3, 27);
+
+  print(
+      "hello i am ${name}. I am ${age} years old. My school fees is ${fees}. My friends names are ${friends}. my dob is ${dob.year}. Do i work ${isworking}");
+}
---
+++ list.dart
@@ -0,0 +1,11 @@
+void main() {
+
+  //list array
+  List<String> friends = ['roshan', 'pranjal', 'ronish', 'kabish', 'yogesh'];
+  List<int> age = [15,22,10,20,50];
+  // numbers.add(10);
+  print('cource name: ${friends[0]}: age ${age[0]}');
+
+
+ 
+}
