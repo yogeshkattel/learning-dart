cde984efd60c5152fcdc355cced5ec44eea5fba5
yogeshkattel <yogeshkattel66@gmail.com>
2021-07-08 16:15:07 +0545

    day2

---
+++ map.dart
@@ -0,0 +1,7 @@
+void main() {
+  Map<String, int> friendsandnumber = {
+    'Roshan':9807930965,
+    'Ronish':9806033057,
+  };
+  print(friendsandnumber["Ronish"]);
+}
---
+++ var.dart
@@ -0,0 +1,8 @@
+void main() {
+  var name = "Nepdev";
+  var address = "NUll";
+  var shares = 4000;
+  var works = ['app developement', 'eb developement', 'cybersec'];
+  var estyear = DateTime(2020,3,21);
+  print(estyear);
+}
